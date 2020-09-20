7d42977257c9cc6dfcc303540e2afaa0a54c81b9
Thales Nascimento <thales.silva.nascimento@gmail.com>
2020-09-20 18:22:53 -0300

    Aulas: Integrando Unform; Usabilidade em Formulários; Foco e preenchimento

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
+  align-self: stretch;
   height: 60px;
   background-color: #ff9000;
   border-radius: 10px;
